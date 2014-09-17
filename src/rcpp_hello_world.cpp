3ffdd7ca90d36a39c7dcc427266afdbac771e515
Harold Pimentel <haroldpimentel@gmail.com>
2014-09-17 16:51:03 -0700

    Initial commit. Rcpp sketeleton

---
+++ src/rcpp_hello_world.cpp
@@ -0,0 +1,16 @@
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' Hello world
+//'
+//' @return a list with foo bar
+// [[Rcpp::export]]
+List rcpp_hello_world() {
+
+    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
+    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
+    List z            = List::create( x, y ) ;
+
+    return z ;
+}
